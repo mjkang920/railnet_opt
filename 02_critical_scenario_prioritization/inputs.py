6e6127115255e2c129c7d38a0cefbd34e08012b4
KANG Minji <156167696+mjkang920@users.noreply.github.com>
2025-10-30 14:56:15 +0900

    link input file and data

---
+++ 02_critical_scenario_prioritization/inputs.py
@@ -0,0 +1,8 @@
+input_files = {
+    "nodes": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\uk\nodes.json",
+    "edges": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\uk\edges.json",
+    "arc_distance": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\uk\arc_distance.json",
+    "intact_capacity": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\uk\intact_capacity.json",
+    "probs": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\uk\probs.json",
+    "demand": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\uk\demand_data.json",
+}
