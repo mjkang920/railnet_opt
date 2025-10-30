a09b376215cae6cf892aef681eb909f04458fa59
KANG Minji <156167696+mjkang920@users.noreply.github.com>
2025-10-30 15:44:51 +0900

    add kor data

--- 02_critical_scenario_prioritization/inputs.py
+++ 02_critical_scenario_prioritization/inputs.py
@@ -1,4 +1,13 @@
-input_files = {
+input_files_kor = {
+    "nodes": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\korea\nodes.json",
+    "edges": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\korea\edges.json",
+    "arc_distance": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\korea\arc_distance.json",
+    "intact_capacity": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\korea\intact_capacity.json",
+    "probs": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\korea\probs.json",
+    "demand": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\korea\demand_data.json",
+}
+
+input_files_uk = {
     "nodes": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\uk\nodes.json",
     "edges": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\uk\edges.json",
     "arc_distance": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\uk\arc_distance.json",
