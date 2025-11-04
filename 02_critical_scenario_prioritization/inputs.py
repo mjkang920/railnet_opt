52d9731b6de9a34ad981cfe15d20a90c8856f481
KANG Minji <156167696+mjkang920@users.noreply.github.com>
2025-11-05 00:28:49 +0900

    update 03_rail_scheduling_dynamic_opt and upload inputs

--- 02_critical_scenario_prioritization/inputs.py
+++ 02_critical_scenario_prioritization/inputs.py
@@ -4,7 +4,7 @@ input_files_kor = {
     "arc_distance": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\korea\arc_distance.json",
     "intact_capacity": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\korea\intact_capacity.json",
     "probs": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\korea\probs.json",
-    "demand": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\korea\demand_data.json",
+    "demand": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\korea\demand_02.json",
 }
 
 input_files_uk = {
@@ -13,5 +13,5 @@ input_files_uk = {
     "arc_distance": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\uk\arc_distance.json",
     "intact_capacity": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\uk\intact_capacity.json",
     "probs": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\uk\probs.json",
-    "demand": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\uk\demand_data.json",
+    "demand": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\uk\demand_02.json",
 }
---
+++ 03_rail_scheduling_dynamic_opt/inputs.py
@@ -0,0 +1,13 @@
+input_files_kor = {
+    "edges":            r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\korea\edges.json",
+    "route":            r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\korea\routes_nodes.json",
+    "dept":             r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\korea\dep_time.json",
+    "demand":           r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\korea\demand_03.json",
+}
+
+input_files_uk = {
+    "edges":            r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\uk\edges.json",
+    "route":            r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\uk\routes_nodes.json",
+    "dept":             r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\uk\dep_time.json",
+    "demand":           r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\uk\demand_03.json",
+}
