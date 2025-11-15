77494af98c78c948051b6067050c21fcf835d0b1
mjkang920 <mjkang920@snu.ac.kr>
2025-11-15 21:08:39 +0900

    remove environment, edit 02_input directory

--- 02_critical_scenario_prioritization/inputs.py
+++ 02_critical_scenario_prioritization/inputs.py
@@ -1,61 +1,66 @@
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+
+DATA_DIR   = PROJECT_ROOT / "01_data"
+KOR_DATA   = DATA_DIR / "korea"
+UK_DATA    = DATA_DIR / "uk"
+
+CRIT_DIR   = PROJECT_ROOT / "02_critical_scenario_prioritization"
+CRIT_OUT   = CRIT_DIR / "outputs"
+
 # network inputs
 input_files_kor = {
-    "nodes": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\korea\nodes.json",
-    "edges": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\korea\edges.json",
-    "arc_distance": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\korea\arc_distance.json",
-    "intact_capacity": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\korea\intact_capacity.json",
-    "probs": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\korea\probs.json",
-    "demand": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\korea\demand_02.json",
+    "nodes":           KOR_DATA / "nodes.json",
+    "edges":           KOR_DATA / "edges.json",
+    "arc_distance":    KOR_DATA / "arc_distance.json",
+    "intact_capacity": KOR_DATA / "intact_capacity.json",
+    "probs":           KOR_DATA / "probs.json",
+    "demand":          KOR_DATA / "demand_02.json",
 }
 
 input_files_uk = {
-    "nodes": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\uk\nodes.json",
-    "edges": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\uk\edges.json",
-    "arc_distance": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\uk\arc_distance.json",
-    "intact_capacity": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\uk\intact_capacity.json",
-    "probs": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\uk\probs.json",
-    "demand": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\uk\demand_02.json",
+    "nodes":           UK_DATA / "nodes.json",
+    "edges":           UK_DATA / "edges.json",
+    "arc_distance":    UK_DATA / "arc_distance.json",
+    "intact_capacity": UK_DATA / "intact_capacity.json",
+    "probs":           UK_DATA / "probs.json",
+    "demand":          UK_DATA / "demand_02.json",
 }
 
-
 # ga inputs
-from pathlib import Path
-
-ROOT = Path(r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\02_critical_scenario_prioritization\outputs")
-
 ga_files_kor = {
-    "delay30": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\02_critical_scenario_prioritization\outputs\kor\Genetic_Algorithm\GA_delay30_mcnf.json",
-    "delay60": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\02_critical_scenario_prioritization\outputs\kor\Genetic_Algorithm\GA_delay60_mcnf.json",
-    "delay120": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\02_critical_scenario_prioritization\outputs\kor\Genetic_Algorithm\GA_delay120_mcnf.json",
+    "delay30": CRIT_OUT / "kor" / "Genetic_Algorithm" / "GA_delay30_mcnf.json",
+    "delay60": CRIT_OUT / "kor" / "Genetic_Algorithm" / "GA_delay60_mcnf.json",
+    "delay120": CRIT_OUT / "kor" / "Genetic_Algorithm" / "GA_delay120_mcnf.json",
 }
 
 ga_files_uk = {
-    "delay30": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\02_critical_scenario_prioritization\outputs\uk\Genetic_Algorithm\GA_delay30_mcnf.json",
-    "delay60": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\02_critical_scenario_prioritization\outputs\uk\Genetic_Algorithm\GA_delay60_mcnf.json",
-    "delay120": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\02_critical_scenario_prioritization\outputs\uk\Genetic_Algorithm\GA_delay120_mcnf.json",
+    "delay30": CRIT_OUT / "uk" / "Genetic_Algorithm" / "GA_delay30_mcnf.json",
+    "delay60": CRIT_OUT / "uk" / "Genetic_Algorithm" / "GA_delay60_mcnf.json",
+    "delay120": CRIT_OUT / "uk" / "Genetic_Algorithm" / "GA_delay120_mcnf.json",
 }
 
 def get_ga_files(region: str = "kor", labels=("delay30", "delay60", "delay120")):
-    src = ga_files_kor if region.lower() == "kor" else ga_files_uk
-    return [(lab, Path(src[lab])) for lab in labels]
+    region = region.lower()
+    src = ga_files_kor if region == "kor" else ga_files_uk
+    return [(lab, src[lab]) for lab in labels]
 
-
-# Basemap
+# basemap
 basemap_files_kor = {
-    "basemap_nodes": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\korea\basemap_nodes.json",
-    "basemap_edges": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\korea\basemap_edges.json",
+    "basemap_nodes": KOR_DATA / "basemap_nodes.json",
+    "basemap_edges": KOR_DATA / "basemap_edges.json",
 }
 
 basemap_files_uk = {
-    "basemap_nodes": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\uk\basemap_nodes.json",
-    "basemap_edges": r"C:\Users\Minji Kang\Documents\GitHub\railnet_opt\01_data\uk\basemap_edges.json",
+    "basemap_nodes": UK_DATA / "basemap_nodes.json",
+    "basemap_edges": UK_DATA / "basemap_edges.json",
 }
 
 def get_basemap_files(region: str = "kor"):
     region = region.lower()
     if region == "kor":
         return basemap_files_kor
-    elif region == "uk":
+    if region == "uk":
         return basemap_files_uk
-    else:
-        raise ValueError("region must be 'kor' or 'uk'")
+    raise ValueError("region must be 'kor' or 'uk'")
