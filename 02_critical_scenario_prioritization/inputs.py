75b47107b69caf1d1ea04399723958d20c088109
KANG Minji <156167696+mjkang920@users.noreply.github.com>
2025-11-17 23:53:02 +0900

    [02] change nodes and edges data to another format

--- 02_critical_scenario_prioritization/inputs.py
+++ 02_critical_scenario_prioritization/inputs.py
@@ -13,18 +13,12 @@ CRIT_OUT   = CRIT_DIR / "outputs"
 input_files_kor = {
     "nodes":           KOR_DATA / "nodes.json",
     "edges":           KOR_DATA / "edges.json",
-    "arc_distance":    KOR_DATA / "arc_distance.json",
-    "intact_capacity": KOR_DATA / "intact_capacity.json",
-    "probs":           KOR_DATA / "probs.json",
     "demand":          KOR_DATA / "demand_02.json",
 }
 
 input_files_uk = {
     "nodes":           UK_DATA / "nodes.json",
     "edges":           UK_DATA / "edges.json",
-    "arc_distance":    UK_DATA / "arc_distance.json",
-    "intact_capacity": UK_DATA / "intact_capacity.json",
-    "probs":           UK_DATA / "probs.json",
     "demand":          UK_DATA / "demand_02.json",
 }
 
@@ -46,21 +40,3 @@ def get_ga_files(region: str = "kor", labels=("delay30", "delay60", "delay120"))
     src = ga_files_kor if region == "kor" else ga_files_uk
     return [(lab, src[lab]) for lab in labels]
 
-# basemap
-basemap_files_kor = {
-    "basemap_nodes": KOR_DATA / "basemap_nodes.json",
-    "basemap_edges": KOR_DATA / "basemap_edges.json",
-}
-
-basemap_files_uk = {
-    "basemap_nodes": UK_DATA / "basemap_nodes.json",
-    "basemap_edges": UK_DATA / "basemap_edges.json",
-}
-
-def get_basemap_files(region: str = "kor"):
-    region = region.lower()
-    if region == "kor":
-        return basemap_files_kor
-    if region == "uk":
-        return basemap_files_uk
-    raise ValueError("region must be 'kor' or 'uk'")
